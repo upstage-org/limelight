f3580fad5dbf8938c4bd0b310abbd0e0911c79d1
rohrlach <paul.rohrlach@gmail.com>
2016-09-15 22:56:21 -0500

    Assigning Media to Stages
    It is now possible to assign media to stages.
    
    - Functionality has been provided on both sides of the relationship for
    users’ convenience.
    
    - Filters are yet in place to omit already assigned records from
    selection boxes.
    
    - Caching should be looked at here as we’re nesting partials and such.
    Will likely be a performance hit, but the code is tidier.

--- app/controllers/stage_media_controller.rb
+++ app/controllers/stage_media_controller.rb
@@ -1,11 +1,11 @@
 class StageMediaController < ApplicationController
   def create
-    if StageMedium.create(params.permit([ :stage_id, :medium_id ]))
+    if StageMedium.create(params.require(:stage_medium).permit([ :stage_id, :medium_id ]))
       flash[:success] = "Media assigned"
     else
       flash[:danger] = "Something went wrong"
     end
-    redirect_to Stage.find_by_id(params[:stage_id])
+    redirect_to Stage.find_by_id(params[:stage_medium][:stage_id])
   end
 
   def destroy
