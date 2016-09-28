1212d99df2a353958735ca0db8ba9d86cd2fa16a
hbp0048 <minjupk@hotmail.com>
2016-09-28 19:54:45 +1300

    Chat Edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20160928062439) do
 
-ActiveRecord::Schema.define(version: 20160922033240) do
   create_table "avatar_stages", force: :cascade do |t|
     t.integer  "stage_id"
     t.integer  "avatar_id"
@@ -47,10 +47,9 @@ ActiveRecord::Schema.define(version: 20160922033240) do
   end
 
   create_table "messages", force: :cascade do |t|
-    t.text     "content"
-    t.string   "user_id"
-    t.string   "integer"
-    t.string   "stage_id"
+    t.string   "content"
+    t.integer  "user_id"
+    t.integer  "stage_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
