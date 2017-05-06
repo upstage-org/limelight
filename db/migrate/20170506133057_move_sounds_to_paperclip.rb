aab308aeb660ec4fcedc558d4ecdd2694a54f237
Paul Rohrlach <paul.rohrlach@matrixbug.com>
2017-05-06 10:16:58 -0400

    Fix Migration Syntax
    As per previous commit

--- db/migrate/20170506133057_move_sounds_to_paperclip.rb
+++ db/migrate/20170506133057_move_sounds_to_paperclip.rb
@@ -2,7 +2,6 @@ class MoveSoundsToPaperclip < ActiveRecord::Migration[5.1]
   def up
     add_attachment :sounds, :source
     remove_belongs_to :sounds, :medium, :foreign_key => true
-    remove_column :sounds, :medium_id
   end
 
   def down
