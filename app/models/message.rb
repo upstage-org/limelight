71b1350ec06107af9a146c280fe5a2cd5a8ca718
rohrlach <paul.rohrlach@matrixbug.com>
2017-05-09 12:34:38 -0400

    Issue #123: Simplify Framework
    Framework will now generate the convience method for the developer

--- app/models/message.rb
+++ app/models/message.rb
@@ -9,19 +9,17 @@ class Message < ApplicationRecord
 
   after_create_commit { MessageBroadcastJob.perform_later(self) }
 
-  def is_shout?
-    self.content.starts_with? MODIFIERS[:shout]
-  end
-
-  def is_thought?
-    self.content.starts_with? MODIFIERS[:thought]
-  end
-
   def formatted_content
     msg = self.content
     if MODIFIERS.map { |m, v| v }.include? msg[0]
       msg[0] = ''
     end
     return msg
+  end
+  
+  MODIFIERS.each do |k, v|
+    define_method("is_#{k.to_s}?") do
+      return self.content.starts_with? v
+    end
   end
 end
