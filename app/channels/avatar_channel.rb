b98de3d6a262325b253eb386c314acc37e71b92f
rohrlach <paul.rohrlach@matrixbug.com>
2017-06-20 13:42:06 -0400

    Server Sanity
    Server will now only accept hold/drop requests if it makes sense to.
    
    * Can only hold if not already being held
    * Can only drop if actually holding

--- app/channels/avatar_channel.rb
+++ app/channels/avatar_channel.rb
@@ -1,20 +1,22 @@
 class AvatarChannel < ApplicationCable::Channel
   def subscribed
     @stage = Stage.find_by_slug!(params[:slug])
+    @avatar_allocation = @stage.avatars.map { |a| [a.id, nil] }.to_h
     stream_for @stage
   end
 
   def hold(data)
-    unless current_user.nil?
-      avatar = @stage.avatars.find_by_id!(data['avatar_id'])
-      AvatarChannel.broadcast_to @stage, { username: current_user.nickname, action: 'hold', avatar_id: avatar.id }
+    debugger
+    unless current_user.nil? || @avatar_allocation[data['avatar_id']] != nil
+      @avatar_allocation[data['avatar_id']] = current_user
+      AvatarChannel.broadcast_to @stage, { username: current_user.nickname, action: 'hold', avatar_id: data['avatar_id'] }
     end
   end
 
   def drop(data)
-    unless current_user.nil?
-      avatar = @stage.avatars.find_by_id!(data['avatar_id'])
-      AvatarChannel.broadcast_to @stage, { action: 'drop', avatar_id: avatar.id }
+    unless current_user.nil? || @avatar_allocation[data['avatar_id']] != current_user
+      @avatar_allocation[data['avatar_id']] = nil
+      AvatarChannel.broadcast_to @stage, { action: 'drop', avatar_id: data['avatar_id'] }
     end
   end
 end
