79b36f64e602a3dde238c348ec1ecd1cff35d967
JoeIceCream <j.earley145@gmail.com>
2017-08-25 14:58:56 +1200

    Final audio update
    fixed indentation
    removed logging (debugging code)

--- app/channels/audio_channel.rb
+++ app/channels/audio_channel.rb
@@ -1,5 +1,5 @@
 class AudioChannel < ApplicationCable::Channel
-	def subscribed
+    def subscribed
     	@stage = Stage.find_by_slug!(params[:slug])
     	@audio_allocation = @stage.sounds.map { |au| [au.source, nil] }.to_h
     	stream_for @stage
@@ -10,5 +10,5 @@ class AudioChannel < ApplicationCable::Channel
     		@audio_allocation[data['audio_id']] = current_user
     		AudioChannel.broadcast_to @stage, { action: 'play', audio_id: data["audio_id"] }
     	end
-  	end
+    end
 end
