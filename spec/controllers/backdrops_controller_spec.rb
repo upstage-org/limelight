2e6fc7b6520414e362d5644fa5838ed4a4eeba45
thongteav <thong.teav@gmail.com>
2017-09-14 11:33:53 +1200

    Backdrop controller rspec
    Added index variables, response, and rendering test

--- spec/controllers/backdrops_controller_spec.rb
+++ spec/controllers/backdrops_controller_spec.rb
@@ -1,47 +1,56 @@
 require 'rails_helper'
-require 'spec_helper'
 
 describe backdrops_controller do
 
-	fixtures :backdrops
+	let(:backdrops) { Backdrop.all }
 
-	describe "#index" do
-		
+	describe "GET #index" do
+		it "assigns all backdrops to @backdrops" do
+			get :index
+			expect(assigns(:backdrops)).to eq(backdrops)
+		end
+
+		it "is a sucess" do
+			expect(response).to have_http_status(:ok)
+		end
+
+		it "render 'index' template" do
+			expect(response).to render_template('backdrops')
+		end
 	end
 
 	describe "#new" do
-		
-	end
-	
-	describe "#create" do
-
-		backdrop = backdrops_controller.create()
-
-		it "should redirect to edit backdrop path" do
-			expect(response).to redirect_to(edit_backdrop_path)
-			expect(flash[:success]).to match"#{@backdrop.name} created"			
-		end
-
-		it "backdrop not uploaded" do
-			#expect(response).to 
-			expect(flash[:danger]).to match"Something went wrong"
-		end
 
 	end
 
-	describe "show" do
-		it "" do
-			
-		end
-	end
-
-	describe "update" do
-		
-	end
-
-	decribe "destroy" do
-		
-	end
-	
-	
+	#
+	# describe "#create" do
+	#
+	# 	backdrop = backdrops_controller.create()
+	#
+	# 	it "should redirect to edit backdrop path" do
+	# 		expect(response).to redirect_to(edit_backdrop_path)
+	# 		expect(flash[:success]).to match"#{@backdrop.name} created"
+	# 	end
+	#
+	# 	it "backdrop not uploaded" do
+	# 		#expect(response).to
+	# 		expect(flash[:danger]).to match"Something went wrong"
+	# 	end
+	#
+	# end
+	#
+	# describe "show" do
+	# 	it "" do
+	#
+	# 	end
+	# end
+	#
+	# describe "update" do
+	#
+	# end
+	#
+	# decribe "destroy" do
+	#
+	# end
 end
