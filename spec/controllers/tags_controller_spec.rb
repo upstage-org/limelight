9698d549d96b7cc507bf0cf664e28de8a5ffb219
Charlie Wang <charlie0605@hotmail.com>
2017-10-08 10:36:05 +1300

    changed format

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -2,51 +2,82 @@ require 'rails_helper'
 
 RSpec.describe TagsController, type: :controller do
 
-  describe "GET #create" do
+  describe "POST #create" do
     before do
       user = User.create({ nickname: 'Admin', email: 'admin@local.instance', password: 'admin', password_confirmation: 'admin', is_active: true, email_confirmed: Time.zone.now })
       cookies[:auth_token] = user.auth_token
     end
 
     describe "when perspective present" do
-      it "should add tag to stage" do
-        stage = Stage.create(name: "test")
-        tag = Tag.create(name: "testing")
-        stage.tags << tag
+      context "when name is without space" do
+        before do
+          @tag = Tag.create(name: "test")
+        end
+        it "should add tag to stage" do
+          stage = Stage.create(name: "test")
+          stage.tags << @tag
 
-        expect(stage.tags).to include(tag)
+          expect(stage.tags).to include(@tag)
+        end
+
+        it "should add tag to backdrop" do
+          backdrop = Backdrop.create(name: "test")
+          backdrop.tags << @tag
+
+          expect(backdrop.tags).to include(@tag)
+        end
+
+        it "should add tag to sound" do
+          sound = Sound.create(name: "test")
+          sound.tags << @tag
+
+          expect(sound.tags).to include(@tag)
+        end
+
+        it "should add tag to avatar" do
+          avatar = Avatar.create(name: "test")
+          avatar.tags << @tag
+
+          expect(avatar.tags).to include(@tag)
+        end
       end
 
-      it "should add tag to stage" do
-        stage = Stage.create(name: "test")
-        tag = Tag.create(name: "testing")
-        stage.tags << tag
+      context "when name is with space" do
+        before do
+          @tag = Tag.create(name: "test space")
+        end
 
-        expect(stage.tags).to include(tag)
-      end
+        it "should add tag to stage" do
+          stage = Stage.create(name: "test")
+          stage.tags << @tag
 
-      it "should add tag to backdrop" do
-        backdrop = Backdrop.create(name: "test")
-        tag = Tag.create(name: "testing")
-        backdrop.tags << tag
+          expect(stage.tags).to include(@tag)
+          expect(@tag.name).to eq("test-space")
+        end
 
-        expect(backdrop.tags).to include(tag)
-      end
+        it "should add tag to backdrop" do
+          backdrop = Backdrop.create(name: "test")
+          backdrop.tags << @tag
 
-      it "should add tag to sound" do
-        sound = Sound.create(name: "test")
-        tag = Tag.create(name: "testing")
-        sound.tags << tag
+          expect(backdrop.tags).to include(@tag)
+          expect(@tag.name).to eq("test-space")
+        end
 
-        expect(sound.tags).to include(tag)
-      end
+        it "should add tag to sound" do
+          sound = Sound.create(name: "test")
+          sound.tags << @tag
 
-      it "should add tag to avatar" do
-        avatar = Avatar.create(name: "test")
-        tag = Tag.create(name: "testing")
-        avatar.tags << tag
+          expect(sound.tags).to include(@tag)
+          expect(@tag.name).to eq("test-space")
+        end
 
-        expect(avatar.tags).to include(tag)
+        it "should add tag to avatar" do
+          avatar = Avatar.create(name: "test")
+          avatar.tags << @tag
+
+          expect(avatar.tags).to include(@tag)
+          expect(@tag.name).to eq("test-space")
+        end
       end
     end
   end
