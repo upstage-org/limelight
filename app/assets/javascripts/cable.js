428887df4a83577c9a02e3aa08a55162a63cbfcb
charlie0605 <charlie0605@hotmail.com>
2017-10-21 18:22:11 +1300

    Update cable.js

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -19,21 +19,18 @@
 
     // Draw backdrop
     App.state.backdrops.forEach(function(backdrop){
-      var newHight;
-      var newWidth;
+      var ratio;
       if(backdrop.image.naturalHeight >= backdrop.image.naturalWidth){
-        var ratio = canvas.height/backdrop.image.naturalHeight;
-        newHight = backdrop.image.naturalHeight * ratio;
-        newWidth = backdrop.image.naturalWidth * ratio;
+        ratio = canvas.height/backdrop.image.naturalHeight;
       } else {
-        var ratio = canvas.width/backdrop.image.naturalWidth;
-        newHight = backdrop.image.naturalHeight * ratio;
-        newWidth = backdrop.image.naturalWidth * ratio;
+        ratio = canvas.width/backdrop.image.naturalWidth;
       }
+      var newHeight = backdrop.image.naturalHeight * ratio;
+      var newWidth = backdrop.image.naturalWidth * ratio;
       var startingX = (canvas.width - newWidth) / 2
-      var startingY = (canvas.height - newHight) / 2
+      var startingY = (canvas.height - newHeight) / 2
 
-      App.context.drawImage(backdrop.image, 0, 0, backdrop.image.naturalWidth, backdrop.image.naturalHeight,startingX,startingY, newWidth,newHight);
+      App.context.drawImage(backdrop.image, 0, 0, backdrop.image.naturalWidth, backdrop.image.naturalHeight,startingX,startingY, newWidth,newHeight);
     });
 
     // Draw avatars
