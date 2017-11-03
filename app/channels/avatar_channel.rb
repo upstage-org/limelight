695c82d816824ad004be393087bcee4a0aeef814
thongteav <thong.teav@gmail.com>
2017-11-04 12:22:21 +1300

    a user can now swap avatars
    -Fixed the problem where the user cannot hold a previously placed avatar

--- app/channels/avatar_channel.rb
+++ app/channels/avatar_channel.rb
@@ -6,7 +6,7 @@ class AvatarChannel < ApplicationCable::Channel
   end
 
   def hold(data)
-    unless current_user.nil? || @avatar_allocation[data['avatar_id']] != nil
+    unless current_user.nil? 
       @avatar_allocation[data['avatar_id']] = current_user
       avatar = Avatar.find_by_id!(data['avatar_id'])
       AvatarChannel.broadcast_to @stage, { username: current_user.id, action: 'hold', avatar_id: data['avatar_id'], file: avatar.source.url(:original) }
@@ -47,9 +47,9 @@ class AvatarChannel < ApplicationCable::Channel
   def size(data)
     unless current_user.nil? || @avatar_allocation[data['avatar_id']] != current_user
       avatar = Avatar.find_by_id!(data['avatar_id'])
-      AvatarChannel.broadcast_to @stage, { 
-        action: 'size', 
-        avatar_id: data['avatar_id'], 
+      AvatarChannel.broadcast_to @stage, {
+        action: 'size',
+        avatar_id: data['avatar_id'],
         value: data['value'],
         file: avatar.source.url(:original)
       }
