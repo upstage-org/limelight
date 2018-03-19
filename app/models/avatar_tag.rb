06230fbd241e6f7f7d68286632f2bf38a2b481ba
alexanderglenn <alexanderjamesglenn@gmail.com>
2018-03-20 11:50:04 +1300

    Table should display backdrop, sound and stage media. Avatars not showing yet

--- app/models/avatar_tag.rb
+++ app/models/avatar_tag.rb
@@ -2,6 +2,6 @@ class AvatarTag < ApplicationRecord
   belongs_to :avatar
   belongs_to :tag
 
-  validates :tag, :presence => true, :uniqueness => { :scope => :avatar }
-  validates :avatar, :presence => true, :uniqueness => { :scope => :tag }
+  validates :tag, :presence => true, :uniqueness => { :scope => :avatar_id }
+  validates :avatar, :presence => true, :uniqueness => { :scope => :tag_id }
 end
