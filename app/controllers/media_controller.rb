ab09f581b4f064ae3807d3e8689ae6e3d045659a
thongteav <thong.teav@gmail.com>
2018-04-19 17:55:43 +1200

    User can search for media items
    -Searching returns any media item with name or tag name matching the search term

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,7 +1,25 @@
 class MediaController < ApplicationController
   before_action :reject_anonymous
   def index
-    @media = Avatar.all + Sound.all + Backdrop.all
+    if params[:term]
+      med = Avatar.all + Sound.all + Backdrop.all
+      @media = Array.new
+      med.each do |m|
+        if m.name.match(params[:term])
+          @media << m
+        else
+          m.tags.each do |t|
+            if t.name.match(params[:term])
+              unless @media.include? m
+                @media << m
+              end
+            end
+          end
+        end
+      end
+    else
+      @media = Avatar.all + Sound.all + Backdrop.all
+    end
     @media.sort_by { |m| m.name }
   end
 end
