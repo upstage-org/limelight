77979e26912d820d3a3b2a9625d0a51033f91d20
thongteav <thong.teav@gmail.com>
2018-04-20 21:37:50 +1200

    Player can add filtering to their search
    - Working filters include: type, stage and tag
    - Changed the form layout to be similar to the old website

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,25 +1,69 @@
 class MediaController < ApplicationController
   before_action :reject_anonymous
   def index
-    if params[:term]
-      med = Avatar.all + Sound.all + Backdrop.all
-      @media = Array.new
+    @media = Array.new
+    avatars = Avatar.all
+    sounds = Sound.all
+    backdrops = Backdrop.all
+    others = Array.new
+
+    if params[:stage].present?
+      avatars = avatars.joins(:stages).where(stages: { id: params[:stage] })
+      sounds = sounds.joins(:stages).where(stages: { id: params[:stage] })
+      backdrops = backdrops.joins(:stages).where(stages: { id: params[:stage] })
+    end
+
+    if params[:tag].present?
+      avatars = avatars.joins(:tags).where(tags: { id: params[:tag] })
+      sounds = sounds.joins(:tags).where(tags: { id: params[:tag] })
+      backdrops = backdrops.joins(:tags).where(tags: { id: params[:tag] })
+    end
+
+    if params[:term].present?
+      avt = avatars.where("avatars.name NOT LIKE ?", "%#{params[:term]}%")
+      s = sounds.where("sounds.name NOT LIKE ?", "%#{params[:term]}%")
+      bd = backdrops.where("backdrops.name NOT LIKE ?", "%#{params[:term]}%")
+
+      avatars = avatars.where("avatars.name LIKE ?", "%#{params[:term]}%")
+      sounds = sounds.where("sounds.name like ?", "%#{params[:term]}%")
+      backdrops = backdrops.where("backdrops.name like ?", "%#{params[:term]}%")
+
+      if params[:type].present?
+        case params[:type]
+        when "Avatar"
+          med = avt
+        when "Backdrop"
+          med = bd
+        when "Sound"
+          med = s
+        end
+      else        
+        med = avt + s + bd
+      end
       med.each do |m|
-        if m.name.match(params[:term])
-          @media << m
-        else
           m.tags.each do |t|
             if t.name.match(params[:term])
-              unless @media.include? m
-                @media << m
+              unless others.include? m
+                others << m
               end
             end
           end
-        end
+      end
+    end
+
+    if params[:type].present?
+      case params[:type]
+      when "Avatar"
+        @media = avatars
+      when "Backdrop"
+        @media = backdrops
+      when "Sound"
+        @media = sounds
       end
     else
-      @media = Avatar.all + Sound.all + Backdrop.all
+      @media = avatars + sounds + backdrops
     end
+    @media += others
     @media.sort_by { |m| m.name }
   end
 end
